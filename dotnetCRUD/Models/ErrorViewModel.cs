bb205bd39cc1c4f6058aa928101fc73018f45c3d
Lucas Pine <mr.pine97@gmail.com>
2022-05-16 16:12:35 -0300

    setup inicial

---
+++ dotnetCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnetCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
