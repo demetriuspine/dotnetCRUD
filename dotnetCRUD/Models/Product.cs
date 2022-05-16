a75d83ea35d9671262c8a5f9b7a1f3e4447ba212
Lucas Pine <mr.pine97@gmail.com>
2022-05-16 16:24:19 -0300

    feat: cria o controller

---
+++ dotnetCRUD/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotnetCRUD.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Column("Id")]
+        [Display(Name = "Código")] // nome que vai aparecer na tela
+        public int Id { get; set; }
+
+        [Column("Name")]
+        [Display(Name = "Name")] // nome que vai aparecer na tela
+        public string Name { get; set; }
+
+        [Column("Quantity")]
+        [Display(Name = "Quantity")] // nome que vai aparecer na tela
+        public int Quantity { get; set; }
+
+    }
+}
