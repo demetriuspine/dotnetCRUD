c98a7eecb27adbf6a01e66d80e19712ded019206
Lucas Pine <mr.pine97@gmail.com>
2022-05-16 16:30:20 -0300

    feat: cria o contexto

---
+++ dotnetCRUD/Models/MyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dotnetCRUD.Models
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+        public DbSet<Product> Product { get; set; }
+
+    }
+}
